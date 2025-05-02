0c8acfe72b67648c8ba9c14255f072a6f772bca7
DanilaBelskiy <belsky32@mail.ru>
2025-05-02 12:23:22 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolo11n.pt")  # load an official model
+
+# Predict with the model
+results = model("src/img.png")  # predict on an image
+
+# Access the results
+for result in results:
+    xywh = result.boxes.xywh  # center-x, center-y, width, height
+    xywhn = result.boxes.xywhn  # normalized
+    xyxy = result.boxes.xyxy  # top-left-x, top-left-y, bottom-right-x, bottom-right-y
+    xyxyn = result.boxes.xyxyn  # normalized
+    names = [result.names[cls.item()] for cls in result.boxes.cls.int()]  # class name of each box
+    confs = result.boxes.conf  # confidence score of each box
+
+    result.show()  # display to screen
