b240a135cc4cc82e9391d0a38cc2ee66c2d261a0
DanilaBelskiy <belsky32@mail.ru>
2025-05-02 13:08:10 +0300

    OOP refactoring

--- main.py
+++ main.py
@@ -1,67 +1,19 @@
-from ultralytics import YOLO
-import cv2
-import os
-
-
-def process_video(input_path, output_path, model):
-    cap = cv2.VideoCapture(input_path)
-    if not cap.isOpened():
-        return 0
-
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    width = int(cap.get(3))
-    height = int(cap.get(4))
-
-    output_path = output_path.rsplit('.', 1)[0] + '.mp4'
-    writer = cv2.VideoWriter(
-        output_path,
-        cv2.VideoWriter_fourcc(*'mp4v'),
-        fps,
-        (width, height)
-    )
-
-    counter = 0
-
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-
-        # Detect
-        results = model.predict(
-            source=frame,
-            classes=[0],   # person
-            conf=0.5,
-            verbose=False
-        )
-
-        # Bounding boxes
-        annotated_frame = results[0].plot()
-        writer.write(annotated_frame)
-
-        # Counter refresh
-        current = len(results[0].boxes)
-        counter = max(counter, current)
-
-    cap.release()
-    writer.release()
-
-    print(f"[DEBUG] Video saved to: {output_path}")
-    print(f"[DEBUG] File exists: {os.path.exists(output_path)}")
-    print(f"[DEBUG] File size: {os.path.getsize(output_path)} bytes")
-
-    return counter
+from model import ModelYOLO
 
 
 if __name__ == "__main__":
 
+    filename = "smol_vid.mp4"
+
     # Input file
-    input_path = "src/vid.mp4"
+    input_path = f"src/{filename}"
 
     # Output file
-    output_path = "result/vid.mp4"
+    output_path = f"result/{filename}"
 
     # Load a model
-    model = YOLO("yolo11n.pt")
+    model = ModelYOLO('yolo11n.pt')
+    model.load_model()
 
-    process_video(input_path, output_path, model)
+    # Process video
+    model.process_video(input_path, output_path)
---
+++ model.py
@@ -0,0 +1,66 @@
+from ultralytics import YOLO
+import cv2
+import os
+
+from datetime import datetime
+
+
+class ModelYOLO:
+    def __init__(self, model_name):
+
+        self.model_name = model_name
+        self.model = None
+        self.classes = [0]   # Person class
+
+    def load_model(self):
+        self.model = YOLO(self.model_name)
+
+    def process_image(self, image):
+        return self.model.predict(source=image, classes=self.classes, conf=0.5, verbose=False)
+
+    def process_video(self, input_path, output_path):
+
+        start_time = datetime.now()
+
+        cap = cv2.VideoCapture(input_path)
+        if not cap.isOpened():
+            return 0
+
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        width = int(cap.get(3))
+        height = int(cap.get(4))
+
+        output_path = output_path.rsplit('.', 1)[0] + '.mp4'
+        writer = cv2.VideoWriter(
+            output_path,
+            cv2.VideoWriter_fourcc(*'mp4v'),
+            fps,
+            (width, height)
+        )
+
+        counter = 0
+
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            # Detect
+            results = self.process_image(frame)
+
+            # Bounding boxes
+            annotated_frame = results[0].plot()
+            writer.write(annotated_frame)
+
+            # Counter refresh
+            current = len(results[0].boxes)
+            counter = max(counter, current)
+
+        cap.release()
+        writer.release()
+
+        print(f"[DEBUG] Video processed")
+        print(f"[DEBUG] Processing time = {datetime.now() - start_time}")
+        print(f"[DEBUG] Result saved to: {output_path}")
+
+        return counter
